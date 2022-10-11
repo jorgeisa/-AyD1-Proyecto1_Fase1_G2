f60b7da59b2df5d9c76cec598983994bfd22f095
Josue Gonzalez <gonzcaal@gmail.com>
2022-10-11 00:51:41 -0600

    Update conexion mysql, add dockerfile

--- Backend/conexion.js
+++ Backend/conexion.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+/**const mysql = require('mysql');
 
 const mysqlConnection = mysql.createConnection({
     host: '34.94.61.196',
@@ -15,5 +15,19 @@ mysqlConnection.connect(function(err){
         console.log('db connected')
     }
 });
+**/
+
+var mysql = require('mysql');
+var mysqlConnection  = mysql.createPool({
+  connectionLimit : 100,
+  waitForConnections : true,
+    queueLimit :0,
+  host            : '34.94.61.196',
+  user            : 'root',
+  password        : 'password',
+  database : 'fase2'
+});
+
+
 
 module.exports = mysqlConnection;
