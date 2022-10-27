558c3faec79eb96b46b2157dd0fb1f51583dbab1
jorgeisa <isaacxicol@gmail.com>
2022-10-26 18:04:46 -0600

    [UPDATE]Frontend
    Actualizado el codigo de frontend.

--- Frontend/full-trip/src/component/NavbarVuelo.js
+++ Frontend/full-trip/src/component/NavbarVuelo.js
@@ -2,7 +2,7 @@ export default function Navbar_Vuelo({ user }) {
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="/">
           Full Trip - {user}
         </a>
         <button
@@ -28,7 +28,7 @@ export default function Navbar_Vuelo({ user }) {
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">
+              <a class="nav-link" href="/">
                 Mostrar vuelos
               </a>
             </li>
--- Frontend/full-trip/src/component/Navbar_Hotel.js
+++ Frontend/full-trip/src/component/Navbar_Hotel.js
@@ -33,7 +33,7 @@ export default function Navbar_Hotel({ user }) {
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="/">
+              <a class="nav-link" href="./">
                 Cerrar Sesion
               </a>
             </li>
